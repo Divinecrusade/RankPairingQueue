b0dd0a8310b7451d47bd91f15b43b57ccff4bac3
DivineCrusade <DivineCrusade@DESKTOP-TFAFPE4>
2023-11-13 13:09:44 +0300

    feat: Declare auxiliry class ForwardList

---
+++ Project/ForwardList.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "IList.hpp"
+
+namespace PriorityQueue
+{
+    template<class ElType, std::equality_comparable Type>
+    class ForwardList : public IList
+    {
+    private:
+
+        struct Node
+        {
+            ElType& element;
+            Node* next{ nullptr };
+        };
+
+    public:
+
+        virtual void insert(ElType const& element, size_t pos = 0U) = 0;
+        virtual ElType& get(size_t pos) = 0;
+        virtual ElType& remove(size_t pos) = 0;
+        virtual ElType& find(Type key) = 0;
+
+    private:
+
+        ForwardList* first{ nullptr };
+        ForwardList* last { nullptr };
+    };
+}
