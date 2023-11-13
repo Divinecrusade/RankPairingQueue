cc5f9a41922b40dc703b60ca8c9230d06252501a
DivineCrusade <DivineCrusade@DESKTOP-TFAFPE4>
2023-11-13 21:41:27 +0300

    feat: Realize Forward List

--- Project/IList.hpp
+++ Project/IList.hpp
@@ -11,8 +11,10 @@ namespace PriorityQueue
         __interface IList
         {
             virtual void insert(ElType const& element, size_t pos = 0U) = 0;
+            virtual void insert_front(ElType const& element) = 0;
+            virtual void insert_back(ElType const& element) = 0;
             virtual ElType& get(size_t pos) = 0;
-            virtual ElType& remove(size_t pos) = 0;
+            virtual void remove(size_t pos) = 0;
         };
     }
 }
