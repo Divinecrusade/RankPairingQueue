d32d92ca0ea2bb4078f70994608260da7c690637
Divinecrusade <gorlovdima@inbox.ru>
2023-11-13 17:47:28 +0300

    feat: Declare class for detail

---
+++ Project/Detail.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "IPriorityElement.hpp"
+
+template<std::equality_comparable Type>
+class Detail : PriorityQueue::IPriorityElement<Type>
+{
+public:
+
+    Detail() = delete;
+    Detail(Type const& id, unsigned priority);
+    Detail(Detail const&) = delete;
+    Detail(Detail&&) = delete;
+
+    Detail& operator=(Detail const&) = delete;
+    Detail& operator=(Detail&&) = delete;
+
+    virtual ~Detail() = default;
+
+    virtual unsigned get_priority() const = 0;
+    virtual void set_priority(int new_priority) = 0;
+    virtual Type const& get_data() const = 0;
+
+private:
+    
+    Type id;
+    unsigned priority;
+};
