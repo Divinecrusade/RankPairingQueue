5ce1647795c464c65ab3102e78ece87793ac5c64
DivineCrusade <DivineCrusade@DESKTOP-TFAFPE4>
2023-11-15 16:31:56 +0300

    fix: Unordered left tree after insertion

--- Project/Unit1.cpp
+++ Project/Unit1.cpp
@@ -6,16 +6,16 @@ int main()
     using namespace PriorityQueue;
     using namespace Auxiliry;
 
-    Detail details1[] { {0, 0}, {0, 1}, {0, 2}, {0, 3}, {0, 4}, {0, 5}, {0, 6}, {0, 7}};
+    Detail details1[] { {0, 0}, {0, 1}, {0, 2}, {0, 3}, {0, 4}, {0, 5}, {0, 6}, {0, 7}, {0, 8} , {0, 9} , {0, 10} };
     RankPairingQueue rpq{};
-    rpq.insert(details1[2U]);
-    rpq.insert(details1[1U]);
-    rpq.insert(details1[5U]);
-    rpq.insert(details1[3U]);
-    rpq.insert(details1[7U]);
-    rpq.insert(details1[6U]);
     rpq.insert(details1[4U]);
-    rpq.insert(details1[0U]);
+    rpq.insert(details1[7U]);
+    rpq.insert(details1[3U]);
+    rpq.insert(details1[5U]);
+    rpq.insert(details1[6U]);
+    rpq.insert(details1[1U]);
+    rpq.insert(details1[8U]);
+    rpq.insert(details1[2U]);
 
 
     std::cout << rpq.minimum().get_priority() << std::endl;
