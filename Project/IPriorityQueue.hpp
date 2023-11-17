129f3afb7172250e1f47921cc707b2689d128e4e
DivineCrusade <DivineCrusade@DESKTOP-TFAFPE4>
2023-11-17 10:32:56 +0300

    refactor: Delete all templates, rewrite dependencies on interfaces

--- Project/IPriorityQueue.hpp
+++ Project/IPriorityQueue.hpp
@@ -6,16 +6,14 @@ namespace PriorityQueue
 {
     namespace Interfaces
     {
-        template<std::equality_comparable Type>
         __interface IPriorityQueue
         {
         public:
 
-            virtual void insert(IPriorityElement<Type> const* element) = 0;
-            virtual IPriorityElement<Type> const* minimum() const = 0;
-            virtual IPriorityElement<Type>* extract_min() = 0;
-            virtual void decrease_key(Type const& data, unsigned priority) = 0;
-            virtual void meld(IPriorityQueue* pq) = 0;
+            virtual void insert(IPriorityElement const& element) = 0;
+            virtual IPriorityElement const& minimum() const = 0;
+            virtual void extract_min() = 0;
+            //virtual void decrease_key(Type const& data, unsigned priority) = 0;
         };
     }
 }
---
+++ Project/TestRankedBinaryTree.cpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "RankedBinaryTree.hpp"
+
+int main()
+{
+    using namespace PriorityQueue;
+    using namespace Auxiliry;
+
+    RankedBinaryTree<int> tree1{ 1 };
+    RankedBinaryTree<int> tree2{ 2 };
+    RankedBinaryTree<int> tree3{ 2 };
+
+
+
+
+    return EXIT_SUCCESS;
+}
